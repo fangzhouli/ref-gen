3febcf41595fedf442ce9a890a6a65d1efd0314f
fangzhouli <fzli0805@gmail.com>
2020-02-06 18:35:24 -0800

    added 2 tep scripts

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     version                         = '1.0.0',
     packages                        = find_packages(),
     entry_points                    = {
-        'console_scripts' : ['ref-gen=refgen.cli.run:main']
+        'console_scripts' : [
+            'ref-gen=refgen.cli.run:main',
+            'ref-find=refgen.cli.internal:ref_find',
+            'ref-download=refgen.cli.internal:ref_download']
     },
     description                     = 'Reference genome extraction package',
     author                          = 'Fangzhou Li',
